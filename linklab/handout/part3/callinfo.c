e82acbf04ae1228d8bdc793a0f6910c9dae0722e
givenone <givenone1@gmail.com>
2019-03-19 16:28:18 +0900

    second

--- linklab/handout/part3/callinfo.c
+++ linklab/handout/part3/callinfo.c
@@ -10,8 +10,18 @@
 //    0         on success
 //   <0         on error
 //
+
+#include <libunwind.h>
+
+
 int get_callinfo(char *fname, size_t fnlen, unsigned long long *ofs)
 {
- 
+  unw_cursor_t cursor;
+  unw_word_t ip, off;
+  int r;
+  unw_context_t context;
+  unw_getcontext(&context);
+ // unw_init_local(&cursor, &context);
+ // r = unw_get_proc_name(&cursor, fname, fnlen, (unw_word_t *) ofs);
   return -1;
 }
